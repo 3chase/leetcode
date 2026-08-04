66d3f9b04d35a7791173e1264ef8ef5695320065
Chase Hays <159822012+3chase@users.noreply.github.com>
2026-08-04 17:21:01 -0500

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+    def encode(self, strs: List[str]) -> str:
+        message = ""
+        for s in strs:
+            message += ('🥺' + str(len(s)) + '🥺' + s )
+        return message
+    def decode(self, s: str) -> List[str]:
+        messages = []
+        i = 0
+        while i < len(s):
+            if s[i] == '🥺':
+                k = i + 1
+                while s[k] != '🥺':
+                    k += 1
+                num = int(s[i+1:k])
+                messages.append(s[k + 1:k + 1 + num])
+            i += len(str(num)) + 2 + num
+        return messages
